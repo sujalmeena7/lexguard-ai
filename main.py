0d2b6551ef056562abcc618855abe65fdd49c854
sujalmeena7 <meenasujal60@gmail.com>
2026-04-27 23:17:14 +0530

    Fix ModuleNotFoundError by migrating to pure LCEL      RAG chain

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 
